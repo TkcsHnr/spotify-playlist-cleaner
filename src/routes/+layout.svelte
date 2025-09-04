3989a905177148de2a50cb2a580191949c3d0c5c
Hunor Takacs <tkcshnr@gmail.com>
2025-09-05 01:02:14 +0200

    prettier avatar in navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -16,19 +16,27 @@
 	<div class="flex-none">
 		{#if data.loggedIn}
 			<div class="dropdown dropdown-end block">
-				<button class="btn btn-circle overflow-clip size-12">
-					<img src={data.userProfile?.images[0].url} alt="User profile" class="w-full h-full" />
+				<button class="btn btn-circle p-1.5 size-12 btn-soft">
+					<img
+						src={data.userProfile?.images[0].url}
+						alt="User profile"
+						class="size-9 object-cover rounded-full"
+					/>
 				</button>
 				<!-- svelte-ignore a11y_no_noninteractive_tabindex -->
 				<div
 					tabindex="0"
-					class="dropdown-content mt-4 bg-base-200 rounded-box border border-base-content/10"
+					class="dropdown-content mt-2 bg-base-300 rounded-box border border-base-content/10"
 				>
 					<ul class="menu min-w-36">
 						<li class="menu-title">{data.userProfile?.display_name}</li>
 						<li>
 							<form action="/api/spotify/logout" method="post" class="p-0 w-full">
-								<input type="submit" value="Log out" class="px-3 py-1.5 cursor-pointer text-error" />
+								<input
+									type="submit"
+									value="Log out"
+									class="px-3 py-1.5 cursor-pointer text-error"
+								/>
 							</form>
 						</li>
 					</ul>
