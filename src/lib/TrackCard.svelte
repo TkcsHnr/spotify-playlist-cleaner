8b22c9dd9e9dcb49ea9fe317925912242aa871f2
Hunor Takacs <tkcshnr@gmail.com>
2025-09-09 11:24:03 +0200

    track card image placeholder

--- src/lib/TrackCard.svelte
+++ src/lib/TrackCard.svelte
@@ -22,11 +22,17 @@
 	class="card w-full max-w-sm rounded-[2rem] shadow-sm bg-base-200 border border-base-content/10"
 >
 	<figure class="px-[1.5rem] pt-[1.5rem]">
-		<img
-			src={track ? track.album.images[0].url : ''}
-			alt="Album cover"
-			class="rounded-[0.5rem] aspect-square w-full bg-base-300"
-		/>
+		{#if track}
+			<img
+				src={track.album.images[0].url}
+				alt="Album cover"
+				class="rounded-[0.5rem] aspect-square w-full bg-base-300"
+			/>
+		{:else}
+			<div class="bg-base-300 w-full aspect-square rounded-[0.5rem] grid justify-center items-center text-base-content/75">
+				Album cover
+			</div>
+		{/if}
 	</figure>
 	<div class="card-body gap-0">
 		<h2 class="card-title">{track ? track.name : 'Track name'}</h2>
